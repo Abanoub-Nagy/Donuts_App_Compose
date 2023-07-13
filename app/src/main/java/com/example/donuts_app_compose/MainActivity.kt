ca26c262e695bc54ad596c102e30c117feee17ab
Abanoub <abanoubnagy24@gmail.com>
2023-07-13 03:55:29 +0300

    make main activity run app

--- app/src/main/java/com/example/donuts_app_compose/MainActivity.kt
+++ app/src/main/java/com/example/donuts_app_compose/MainActivity.kt
@@ -3,14 +3,12 @@ package com.example.donuts_app_compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import com.example.donuts_app_compose.ui.theme.Donuts_App_ComposeTheme
+import com.example.donuts_app_compose.ui.theme.spacing
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,28 +17,14 @@ class MainActivity : ComponentActivity() {
             Donuts_App_ComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
+                    modifier = Modifier.padding(
+                        MaterialTheme.spacing._8
+                    ),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DonutApp()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    Donuts_App_ComposeTheme {
-        Greeting("Android")
     }
 }
