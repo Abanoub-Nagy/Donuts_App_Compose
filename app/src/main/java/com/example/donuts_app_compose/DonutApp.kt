7788f35261a4af52c357563d07760ea85cb69583
Abanoub <abanoubnagy24@gmail.com>
2023-07-13 03:53:56 +0300

    initial donut app file

---
+++ app/src/main/java/com/example/donuts_app_compose/DonutApp.kt
@@ -0,0 +1,11 @@
+package com.example.donuts_app_compose
+
+import androidx.compose.runtime.Composable
+import com.example.donuts_app_compose.ui.theme.Donuts_App_ComposeTheme
+
+@Composable
+fun DonutApp() {
+    Donuts_App_ComposeTheme {
+
+    }
+}
