3fad428c817a608323ffe6484183c370ce07f517
Abanoub <abanoubnagy24@gmail.com>
2023-07-13 03:49:36 +0300

    add typography to project

--- app/src/main/java/com/example/donuts_app_compose/ui/theme/Type.kt
+++ app/src/main/java/com/example/donuts_app_compose/ui/theme/Type.kt
@@ -2,33 +2,98 @@ package com.example.donuts_app_compose.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.example.donuts_app_compose.R
 
-// Set of Material typography styles to start with
+val interFontFamily = FontFamily(Font(R.font.inter))
+
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 16.sp,
-        lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
+    headlineLarge = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 54.sp,
+        color = Pink90
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 30.sp,
+        color = Pink90
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        color = Black
+    ),
     titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        color = Black80
+    ),
+    titleMedium = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        color = Black
+    ),
+    titleSmall = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        color = Black60
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Medium,
+        fontSize = 18.sp,
+        color = Pink60
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = interFontFamily,
         fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        color = Black60
+    ),
+    bodySmall = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        color = Black60
+    ),
+    displayLarge = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 30.sp,
+        color = Black
+    ),
+    displayMedium = TextStyle(
+        fontFamily = interFontFamily,
         fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        color = Black
+    ),
+    displaySmall = TextStyle(
+        fontFamily = interFontFamily,
+        fontSize = 14.sp,
+    ),
+    labelLarge = TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+    ),
+    labelMedium =TextStyle(
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
+        fontFamily = interFontFamily,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+    ),
+
 )
