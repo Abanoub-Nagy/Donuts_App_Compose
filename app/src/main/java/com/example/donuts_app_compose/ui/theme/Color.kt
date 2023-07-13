82613d7320cde4d57d370180c35c5364541ebead
Abanoub <abanoubnagy24@gmail.com>
2023-07-13 03:49:09 +0300

    add color to project

--- app/src/main/java/com/example/donuts_app_compose/ui/theme/Color.kt
+++ app/src/main/java/com/example/donuts_app_compose/ui/theme/Color.kt
@@ -9,3 +9,19 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val Pink90 = Color(0xFFFF7074)
+val Pink60 = Color(0xFFFF9494)
+val Pink30 = Color(0xFFFED8DF)
+
+val BlueCard = Color(0xFFD7E4F6)
+val PinkCard = Color(0xFFFFC7D0)
+
+val Background = Color(0xFFFDFDFD)
+val SecondaryColor = Color(0xFF585858)
+
+val White = Color(0xFFFFFFFF)
+
+val Black = Color(0xFF000000)
+val Black80 = Color(0x50000000)
+val Black60 = Color(0x3C000000)
