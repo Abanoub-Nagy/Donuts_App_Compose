e71d8d9a411ea17dedb710a915dbcd28581d139f
Abanoub <abanoubnagy24@gmail.com>
2023-07-18 08:13:20 +0300

    implement details ui sta

--- app/src/main/java/com/example/donuts_app_compose/ui/screens/details/DetailsUiState.kt
+++ app/src/main/java/com/example/donuts_app_compose/ui/screens/details/DetailsUiState.kt
@@ -1,2 +1,14 @@
 package com.example.donuts_app_compose.ui.screens.details
 
+import androidx.compose.ui.graphics.Color
+import com.example.donuts_app_compose.ui.theme.Pink30
+
+data class DetailsUiState(
+    val image: Int = 0,
+    val backgroundColor: Color = Pink30,
+    val favorite: Boolean = false,
+    val title: String = "",
+    val description: String = "",
+    val price: Int = 0,
+    val quantity: Int = 1
+)
