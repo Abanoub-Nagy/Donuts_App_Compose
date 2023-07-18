0662ece73d6047fb6a79b75e16f375efdb30406c
Abanoub <abanoubnagy24@gmail.com>
2023-07-18 08:14:12 +0300

    implement details listener

---
+++ app/src/main/java/com/example/donuts_app_compose/ui/screens/details/DetailsInteractionListener.kt
@@ -0,0 +1,9 @@
+package com.example.donuts_app_compose.ui.screens.details
+
+interface DetailsInteractionListener {
+    fun onClickCardFavoriteIcon()
+
+    fun onClickPlusIcon()
+
+    fun onClickMinusIcon()
+}
