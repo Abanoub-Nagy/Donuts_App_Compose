18347b382d3aab191e32552a7c4cbaf7b1a6433f
Abanoub <abanoubnagy24@gmail.com>
2023-07-18 08:04:43 +0300

    implement home ui state

--- app/src/main/java/com/example/donuts_app_compose/ui/screens/home/HomeUiState.kt
+++ app/src/main/java/com/example/donuts_app_compose/ui/screens/home/HomeUiState.kt
@@ -1,4 +1,21 @@
 package com.example.donuts_app_compose.ui.screens.home
 
-class HomeUiState {
-}
+data class HomeUiState(
+    val todayOffer: List<TodayOfferUiState> = emptyList(),
+    val donuts: List<DonutsUiState> = emptyList()
+)
+
+data class TodayOfferUiState(
+    val favouriteIcon: Boolean = false,
+    val image: Int = 0,
+    val title: String = "",
+    val description: String = "",
+    val originalPrice: Int = 0,
+    val discountedPrice: Int = 0
+)
+
+data class DonutsUiState(
+    val image: Int = 0,
+    val title: String = "",
+    val price: Int = 0
+)
